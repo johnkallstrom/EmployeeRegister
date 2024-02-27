2491ac5db1ae0438bd4de890f693fcd3fe8ac8c3
John Källström <johnkaellstrom@gmail.com>
2024-02-27 15:39:34 +0100

    Create instance of repository, call utility methods and getting user menu choice

--- EmployeeRegister/Program.cs
+++ EmployeeRegister/Program.cs
@@ -4,7 +4,26 @@
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Employee Register");
+			var repository = new EmployeeRepository();
+			repository.Initialize();
+
+			Utilities.DisplayMenu();
+			int choice = Utilities.GetChoice(1, 2);
+            Console.WriteLine();
+
+			if (choice == 1)
+			{
+				var employee = Utilities.AddEmployee();
+				if (employee is not null)
+				{
+					repository.Add(employee);
+				}
+			}
+            if (choice == 2)
+			{
+				var employees = repository.GetAll();
+				Utilities.DisplayEmployeeList(employees);
+			}
         }
 	}
 }
