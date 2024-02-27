a687c6b92a9e96d0bc93eb5074cdaaf66b4301cc
John Källström <johnkaellstrom@gmail.com>
2024-02-27 16:42:12 +0100

    Unit tests

---
+++ EmployeeRegister.Tests/EmployeeRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeRegister.Models;
+using EmployeeRegister.Repositories;
+using FluentAssertions;
+
+namespace EmployeeRegister.Tests
+{
+	public class EmployeeRepositoryTests
+	{
+        [Fact]
+		public void GetAll_Should_ReturnListOfTypeEmployeeModel()
+		{
+			var repository = new EmployeeRepository();
+
+			var employees = repository.GetAll();
+
+			employees.Should().AllBeOfType<EmployeeModel>();
+		}
+
+		[Fact]
+        public void GetAll_Should_ReturnNotEmptyOrNullList()
+        {
+            var repository = new EmployeeRepository();
+
+            var employees = repository.GetAll();
+
+            employees.Should().NotBeNullOrEmpty();
+        }
+	}
+}
