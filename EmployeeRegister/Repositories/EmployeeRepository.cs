6229e3b46144fbe0b4fc70581dd0135f9edb6476
John Källström <johnkaellstrom@gmail.com>
2024-02-27 15:03:22 +0100

    Repository

---
+++ EmployeeRegister/Repositories/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace EmployeeRegister.Repositories
+{
+	public class EmployeeRepository : IEmployeeRepository
+	{
+		private List<EmployeeModel> _employees = new();
+
+		public void Initialize()
+		{
+			_employees.Add(new EmployeeModel("Bob Frapples", 34000, "SEK"));
+			_employees.Add(new EmployeeModel("John Doe", 29000, "SEK"));
+			_employees.Add(new EmployeeModel("Susan Collins", 37000, "SEK"));
+		}
+
+		public List<EmployeeModel> GetAll()
+		{
+			return _employees;
+		}
+
+		public void Add(string name, int salary, string currency)
+		{
+			_employees.Add(new EmployeeModel(name, salary, currency));
+		}
+	}
+}
---
+++ EmployeeRegister/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeRegister.Repositories
+{
+	public interface IEmployeeRepository
+	{
+		void Initialize();
+		List<EmployeeModel> GetAll();
+		void Add(string name, int salary, string currency);
+	}
+}
